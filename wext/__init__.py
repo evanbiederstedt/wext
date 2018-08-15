4612a70b88cdc9110099d277b002b07355e98eac
Evan Biederstedt <evan.biederstedt@gmail.com>
2018-08-15 03:55:35 -0400

    revise using python3.x syntax for explicit relative imports

--- wext/__init__.py
+++ wext/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 # Import modules.
-from constants import *
-from statistics import *
-from i_o import *
-from enumerate_sets import *
-from mcmc import mcmc
-from exact import exact_test
+from .constants import *
+from .statistics import *
+from .i_o import *
+from .enumerate_sets import *
+from .mcmc import mcmc
+from .exact import exact_test
 import cpoibin
-from saddlepoint import saddlepoint
+from .saddlepoint import saddlepoint
 from comet_exact_test import comet_exact_test
-from exclusivity_tests import re_test, wre_test
+from .exclusivity_tests import re_test, wre_test
 from bipartite_edge_swap_module import bipartite_edge_swap
