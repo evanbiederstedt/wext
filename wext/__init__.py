38acd11da31d97db9bbe38bc9b4bc17979be0968
evanbiederstedt <evan.biederstedt@gmail.com>
2018-09-10 10:19:52 -0400

    these should be global modules

--- wext/__init__.py
+++ wext/__init__.py
@@ -7,9 +7,9 @@ from .i_o import *
 from .enumerate_sets import *
 from .mcmc import mcmc
 from .exact import exact_test
-from .src.c import cpoibin
-from .src.c import wext_exact_test 
+import cpoibin
+import wext_exact_test 
 from .saddlepoint import saddlepoint
-from .src.c import comet_exact_tests
+import comet_exact_tests
 from .exclusivity_tests import re_test, wre_test
-from .src.fortran import bipartite_edge_swap_module
+from bipartite_edge_swap_module import bipartite_edge_swap
