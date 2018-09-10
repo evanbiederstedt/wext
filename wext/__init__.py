61fc045ea33fff1bd6c925611823ebb83f49d429
evanbiederstedt <evan.biederstedt@gmail.com>
2018-09-09 20:03:11 -0400

    revised __init__.py to correclty import modules

--- wext/__init__.py
+++ wext/__init__.py
@@ -8,7 +8,9 @@ from .enumerate_sets import *
 from .mcmc import mcmc
 from .exact import exact_test
 import cpoibin
+import wext_exact_test 
+import comet_exact_tests
 from .saddlepoint import saddlepoint
-from comet_exact_test import comet_exact_test
+from comet_exact_tests import comet_exact_test
 from .exclusivity_tests import re_test, wre_test
 from bipartite_edge_swap_module import bipartite_edge_swap
